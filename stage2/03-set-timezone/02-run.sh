52684176166e9d3efe9ff754b1ef3d2ca1572769
132ikl <132@ikl.sh>
2019-07-22 23:00:05 -0400

    Switch TravisCI to use Docker build, fix timezone

--- stage2/03-set-timezone/02-run.sh
+++ stage2/03-set-timezone/02-run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-echo "America/New York" > "${ROOTFS_DIR}/etc/timezone"
+echo "America/New_York" > "${ROOTFS_DIR}/etc/timezone"
 rm "${ROOTFS_DIR}/etc/localtime"
 
 on_chroot << EOF
