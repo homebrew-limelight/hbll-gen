83aa375d05bbfcd5045c3980d4d93a063bed9d85
132ikl <132@ikl.sh>
2019-10-05 08:22:07 -0400

    Add new package targz format

--- stage3/00-opensight/10-run-chroot.sh
+++ stage3/00-opensight/10-run-chroot.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
 # TODO: don't force overwrite
-apt -o Dpkg::Options::="--force-overwrite" install -y /packages/*.deb
+apt -o Dpkg::Options::="--force-overwrite" install -y /packages/deps/*.deb
 rm -rf /packages/
