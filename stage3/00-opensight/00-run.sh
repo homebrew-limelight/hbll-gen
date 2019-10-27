b40dc5d0c54f6115d43010f3dc32dd674fb9c871
132ikl <132@ikl.sh>
2019-10-27 00:54:41 -0400

    Add example nodetrees

--- stage3/00-opensight/00-run.sh
+++ stage3/00-opensight/00-run.sh
@@ -4,3 +4,6 @@ url="$(curl https://api.github.com/repos/opensight-cv/packages/releases/latest |
 curl -LO $url
 mkdir -p ${ROOTFS_DIR}/packages
 tar xf opsi-packages-*.tar.gz -C ${ROOTFS_DIR}/packages
+
+mkdir -p ${ROOTFS_DIR}/boot/opensight/
+cp -rf nodetrees/ ${ROOTFS_DIR}/boot/opensight/
