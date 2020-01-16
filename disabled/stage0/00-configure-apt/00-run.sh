d3bf9d800140ef4d789e37dbb6c0fd4ed48ab8b5
132ikl <132@ikl.sh>
2020-01-16 16:16:10 -0500

    Start test arm64 image

--- disabled/stage0/00-configure-apt/00-run.sh
+++ disabled/stage0/00-configure-apt/00-run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 
 install -m 644 files/sources.list "${ROOTFS_DIR}/etc/apt/"
+install -m 644 files/raspi.list "${ROOTFS_DIR}/etc/apt/sources.list.d/"
 
 if [ -n "$APT_PROXY" ]; then
 	install -m 644 files/51cache "${ROOTFS_DIR}/etc/apt/apt.conf.d/51cache"
@@ -9,7 +10,7 @@ else
 	rm -f "${ROOTFS_DIR}/etc/apt/apt.conf.d/51cache"
 fi
 
-# on_chroot apt-key add - < files/raspberrypi.gpg.key
+on_chroot apt-key add - < files/raspberrypi.gpg.key
 on_chroot << EOF
 apt-get update
 apt-get dist-upgrade -y
--- stage2/01-sys-tweaks/01-run.sh
+++ stage2/01-sys-tweaks/01-run.sh
@@ -5,7 +5,7 @@ install -m 755 files/resize2fs_once	"${ROOTFS_DIR}/etc/init.d/"
 install -d				"${ROOTFS_DIR}/etc/systemd/system/rc-local.service.d"
 install -m 644 files/ttyoutput.conf	"${ROOTFS_DIR}/etc/systemd/system/rc-local.service.d/"
 
-install -m 644 files/50raspi		"${ROOTFS_DIR}/etc/apt/apt.conf.d/"
+# install -m 644 files/50raspi		"${ROOTFS_DIR}/etc/apt/apt.conf.d/"
 
 install -m 644 files/console-setup   	"${ROOTFS_DIR}/etc/default/"
 
@@ -20,7 +20,6 @@ if [ "${ENABLE_SSH}" == "1" ]; then
 else
 	systemctl disable ssh
 fi
-systemctl enable regenerate_ssh_host_keys
 EOF
 
 if [ "${USE_QEMU}" = "1" ]; then
