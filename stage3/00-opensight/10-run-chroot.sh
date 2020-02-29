c91880c14f88bfc1f58b36d56119f610d0c54f0b
132ikl <132@ikl.sh>
2020-02-29 00:17:13 -0500

    Install suggests packages

--- stage3/00-opensight/10-run-chroot.sh
+++ stage3/00-opensight/10-run-chroot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 # TODO: don't force overwrite
-apt -o Dpkg::Options::="--force-overwrite" install -y /packages/deps/*.deb
+apt -o Dpkg::Options::="--force-overwrite" -y --install-suggests install /packages/deps/*.deb
 rm -rf /packages/
 
 systemctl mask rpi-eeprom-update
