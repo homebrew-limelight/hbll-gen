4f74f5dab45e4a0785c8c438267d73634c08a0c1
132ikl <132@ikl.sh>
2020-02-29 23:23:26 -0500

    Only install main OpenSight deb suggests

--- stage3/00-opensight/10-run-chroot.sh
+++ stage3/00-opensight/10-run-chroot.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -e
 
 # TODO: don't force overwrite
-apt -o Dpkg::Options::="--force-overwrite" -y --install-suggests install /packages/deps/*.deb
+suggests=$(dpkg-deb -I "/packages/deps/opensight_"*".deb" | grep Suggests | sed -e 's/ Suggests: //' -e 's/:.*$//g' -e 's/\n/ /g' -e 's/(/@/g' -e 's/)/@/g' -e 's/ @\([^@]*\)@//g' -e "s/,//g")
+apt -o Dpkg::Options::="--force-overwrite" -y install /packages/deps/*.deb $suggests
 rm -rf /packages/
 
 systemctl mask rpi-eeprom-update
